760980af629031ecd94abe41fd42f29bd9c74331
rtegg <robin.tegg@lexisnexisrisk.com>
2023-07-10 10:11:17 +0100

    pagination

--- src/test/java/com/robintegg/web/pages/IndexPageTest.java
+++ src/test/java/com/robintegg/web/pages/IndexPageTest.java
@@ -2,6 +2,7 @@ package com.robintegg.web.pages;
 
 import com.robintegg.web.engine.ContentModel;
 import com.robintegg.web.engine.Page;
+import com.robintegg.web.engine.Pageable;
 import com.robintegg.web.theme.pages.IndexPage;
 import j2html.TagCreator;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,7 @@ class IndexPageTest {
     @Test
     void shouldRender() {
 
-        Page page = IndexPage.create( "index.html", 1, 10, true, true);
+        Page page = IndexPage.create("index.html", new Pageable(1, 10));
 
         System.out.println(
                 TagCreator.html(
