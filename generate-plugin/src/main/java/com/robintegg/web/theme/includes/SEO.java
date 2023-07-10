d89159a6b1a2bb5ecf0545d778033710d93f58f6
rtegg <robin.tegg@lexisnexisrisk.com>
2023-07-10 15:01:53 +0100

    fix some build warnings

--- generate-plugin/src/main/java/com/robintegg/web/theme/includes/SEO.java
+++ generate-plugin/src/main/java/com/robintegg/web/theme/includes/SEO.java
@@ -1,7 +1,5 @@
 package com.robintegg.web.theme.includes;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -9,12 +7,18 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.robintegg.web.engine.ContentModel;
 import j2html.tags.DomContent;
 import j2html.tags.UnescapedText;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 import static j2html.TagCreator.*;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class SEO {
 
-    private static ObjectMapper objectMapper = null;
+    public static final String PROPERTY = "property";
+    public static final String DESCRIPTION = "description";
+    private static final ObjectMapper objectMapper;
 
     static {
 
@@ -34,31 +38,31 @@ public class SEO {
                 meta().withName("generator").withContent("j2html"),
                 iffElse(
                         contentModel.getPage().getTitle() != null,
-                        meta().attr("property", "og:title").withContent(contentModel.getPage().getTitle()),
-                        meta().attr("property", "og:title").withContent(contentModel.getSite().getTitle())
+                        meta().attr(PROPERTY, "og:title").withContent(contentModel.getPage().getTitle()),
+                        meta().attr(PROPERTY, "og:title").withContent(contentModel.getSite().getTitle())
                 ),
                 meta().withName("author").withContent(contentModel.getSite().getAuthor().getName()),
-                meta().attr("property", "og:locale").withContent(contentModel.getLang()),
+                meta().attr(PROPERTY, "og:locale").withContent(contentModel.getLang()),
                 iffElse(
                         contentModel.getPage().getExcerpt() != null,
-                        meta().withName("description").withContent(contentModel.getPage().getExcerpt()),
-                        meta().withName("description").withContent(contentModel.getSite().getDescription())
+                        meta().withName(DESCRIPTION).withContent(contentModel.getPage().getExcerpt()),
+                        meta().withName(DESCRIPTION).withContent(contentModel.getSite().getDescription())
                 ),
                 iffElse(
                         contentModel.getPage().getExcerpt() != null,
-                        meta().attr("property", "og:description").withContent(contentModel.getPage().getExcerpt()),
-                        meta().attr("property", "og:description").withContent(contentModel.getSite().getDescription())
+                        meta().attr(PROPERTY, "og:description").withContent(contentModel.getPage().getExcerpt()),
+                        meta().attr(PROPERTY, "og:description").withContent(contentModel.getSite().getDescription())
                 ),
                 link().withRel("canonical").withHref(contentModel.getSite().resolveUrl(contentModel.getPage().getUrl())),
-                meta().attr("property", "og:url").withContent(contentModel.getSite().resolveUrl(contentModel.getPage().getUrl())),
-                meta().attr("property", "og:site_name").withContent(contentModel.getSite().getTitle()),
+                meta().attr(PROPERTY, "og:url").withContent(contentModel.getSite().resolveUrl(contentModel.getPage().getUrl())),
+                meta().attr(PROPERTY, "og:site_name").withContent(contentModel.getSite().getTitle()),
                 iff(
                         contentModel.getPage().getImageUrl() != null,
-                        meta().attr("property", "og:image").withContent(contentModel.getPage().getImageUrl())
+                        meta().attr(PROPERTY, "og:image").withContent(contentModel.getPage().getImageUrl())
                 ),
-                meta().attr("property", "og:type").withContent("website"),
+                meta().attr(PROPERTY, "og:type").withContent("website"),
                 meta().withName("twitter:card").withContent("summary"),
-                meta().attr("property", "twitter:title").withContent(contentModel.getSite().getAuthor().getName()),
+                meta().attr(PROPERTY, "twitter:title").withContent(contentModel.getSite().getAuthor().getName()),
                 meta().withName("twitter:site").withContent("@" + contentModel.getSite().getTwitterUsername()),
                 meta().withName("twitter:creator").withContent("@" + contentModel.getSite().getTwitterUsername()),
                 script()
@@ -69,6 +73,7 @@ public class SEO {
         );
     }
 
+    @SneakyThrows
     private static String ldJson(ContentModel contentModel) {
 
         ObjectNode author = objectMapper.createObjectNode();
@@ -78,17 +83,13 @@ public class SEO {
         ObjectNode jsonObject = objectMapper.createObjectNode();
         jsonObject.put("@context", "https://schema.org");
         jsonObject.put("@type", "WebSite");
-        jsonObject.put("author", author);
-        jsonObject.put("description", contentModel.getSite().getDescription());
+        jsonObject.set("author", author);
+        jsonObject.put(DESCRIPTION, contentModel.getSite().getDescription());
         jsonObject.put("headline", contentModel.getSite().getTitle());
         jsonObject.put("name", contentModel.getSite().getTitle());
         jsonObject.put("url", contentModel.getSite().getUrl());
 
-        try {
-            return objectMapper.writeValueAsString( jsonObject );
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
+        return objectMapper.writeValueAsString(jsonObject);
 
     }
 }
